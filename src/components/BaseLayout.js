e529ed8d675288dbeb4a64489aa61d5373c73917
visidore <victorien.isidore@gmail.com>
2022-12-29 21:14:06 -0500

    Adding modifications on the templat

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,38 +1,38 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Style from './BaseLayout.module.scss'
 import Navbar from "./Navbar";
 import Home from "./home/Home";
 import About from "./about/About";
 import Portfolio from "./portfolio/Portfolio";
-import {Route, Routes} from "react-router-dom";
-import {Box, Grid} from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import { Box, Grid } from "@mui/material";
 
 export default function BaseLayout() {
-   let [darkMode, setDarkMode] = useState(false);
+   let [darkMode, setDarkMode] = useState(true);
 
    function handleClick() {
       setDarkMode(!darkMode);
    }
 
    return (
-      <Box className={darkMode ? Style.dark : Style.light}>
+      <Box className={darkMode ? Style.dark : Style.light} style={{ padding: '30px' }}>
          <Grid container display={'flex'} flexDirection={'column'} minHeight={'100vh'}
-               justifyContent={'space-between'}>
+            justifyContent={'space-between'}>
             <Grid item>
-               <Navbar darkMode={darkMode} handleClick={handleClick}/>
+               <Navbar darkMode={darkMode} handleClick={handleClick} />
             </Grid>
             <Grid item flexGrow={1}>
                <Routes>
-                  <Route exact path={'/'} element={<Home/>}/>
-                  <Route exact path={'/about'} element={<About/>}/>
-                  <Route exact path={'/portfolio'} element={<Portfolio/>}/>
+                  <Route exact path={'/'} element={<Home />} />
+                  <Route exact path={'/about'} element={<About />} />
+                  <Route exact path={'/projects'} element={<Portfolio />} />
                </Routes>
             </Grid>
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
-                    py={'1.5rem'} sx={{opacity: 0.7}} width={'100%'}>
-                  <p>template created with &hearts; by <a href={'https://paytonpierce.dev'}>Payton Pierce</a></p>
-                  <p>&copy; 2022</p>
+                  py={'1.5rem'} sx={{ opacity: 0.7 }} width={'100%'}>
+                  <p>Template made by <a href={'https://paytonpierce.dev'}>Payton Pierce</a></p>
+                  <p>&copy; 2023</p>
                </Box>
             </Grid>
          </Grid>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Style from './Navbar.module.scss';
-import Toggler from "./home/Toggler";
-import {Link, useLocation} from "react-router-dom";
-import {Box} from "@mui/material";
-import {info} from "../info/Info";
+import { Link, useLocation } from "react-router-dom";
+import { Box } from "@mui/material";
+import { info } from "../info/Info";
 
 const links = [
     {
@@ -17,39 +16,30 @@ const links = [
         active: 'about'
     },
     {
-        name: info.initials,
-        type: 'initials',
-        to: '/',
-        active: 'home'
-    },
-    {
-        name: 'Portfolio',
-        to: '/portfolio',
+        name: 'Projects',
+        to: '/projects',
         active: 'portfolio'
     }
 ]
 
-export default function Navbar({darkMode, handleClick}) {
+export default function Navbar({ darkMode, handleClick }) {
     const location = useLocation()
     const [active, setActive] = useState(location.pathname === '/' ? 'home' : location.pathname.slice(1, location.pathname.length));
 
     return (
         <Box component={'nav'} width={'100%'}>
             <Box component={'ul'} display={'flex'} justifyContent={'center'} alignItems={'center'}
-                 gap={{xs: '2rem', md: '8rem'}}
-                 textTransform={'lowercase'} fontSize={'1rem'}>
+                gap={{ xs: '2rem', md: '8rem' }}
+                fontSize={'1rem'}>
                 {links.map((link, index) => (
                     <Box key={index} component={'li'} className={(link.active === active && !link.type) && Style.active}
-                         sx={{borderImageSource: info.gradient}}>
+                        sx={{ borderImageSource: info.gradient }}>
                         <Link to={link.to} onClick={() => setActive(link.active)}>
-                            {!link.type && <p style={{paddingBottom: '0.5rem'}}>{link.name}</p>}
+                            {!link.type && <p style={{ paddingBottom: '0.5rem' }}><h1>{link.name}</h1></p>}
                             {link.type && <h1>{link.name}</h1>}
                         </Link>
                     </Box>
                 ))}
-                <li>
-                    <Toggler darkMode={darkMode} handleClick={handleClick}/>
-                </li>
             </Box>
         </Box>
     )
